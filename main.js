b03612fac22763d7f7b8ed0831d067cf86fda4fd
GorkemEldeniz <gorkemeldeniz30@gmail.com>
2022-06-22 19:27:54 +0300

    fix the bug

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ const form = document.querySelector('form')
 let todoList = document.querySelector('.todoList')
 const input = document.querySelector('form > input[type="text"]')
 
-let todos = []
+
 window.addEventListener('DOMContentLoaded',e => {
+    let todos = JSON.parse(localStorage.getItem('todos')) || []
     form.addEventListener('submit', e => {
         e.preventDefault()
         //veriler al
         const data = {...Object.fromEntries(new FormData(e.target)),checked : false, time : Date.now(), id:todos.length}
         todos.push(data)
+        localStorage.setItem('todos',JSON.stringify(todos))
         displayTodos()
         input.value = ''    
     })
-
+    displayTodos()
     function displayTodos(){
         
         todoList = document.querySelector('.todoList')
@@ -22,8 +24,8 @@ window.addEventListener('DOMContentLoaded',e => {
             todo.classList.add('todo')
         
             const span = document.createElement('span')
-            span.textContent = el.task
-        
+            span.innerHTML= `<input value = ${el.task} readonly type='text'/>`
+            
             const actionDiv = document.createElement('div')
             actionDiv.classList.add('actions')
         
@@ -49,15 +51,38 @@ window.addEventListener('DOMContentLoaded',e => {
         
             todoList.appendChild(todo)
 
-            checkbox.addEventListener('checked',e => {
-                if(!el.checked) el.checked = true
-                else el.checked = false
-                span.classList.toggle('checked',el.checked)
+            checkbox.addEventListener('input',e => {
+                span.children[0].classList.toggle('checked',checkbox.checked)
+                todos.forEach(todo => {
+                    if(todo == el) {
+                        todo.checked = checkbox.checked
+                    }
+                })
+                localStorage.setItem('todos',JSON.stringify(todos))
             })
 
             deleteBtn.addEventListener('click' ,e => {
+                e.preventDefault()
                 todos = todos.filter(t => t != el)
+                localStorage.setItem('todos',JSON.stringify(todos))
                 displayTodos()
+            })
+
+
+            editBtn.addEventListener('click', e => {
+                e.preventDefault()
+                span.children[0].removeAttribute('readonly')
+                span.children[0].focus()
+                span.children[0].addEventListener('blur',e => {
+                    todos.map(todo => {
+                        if(todo == el) {
+                            el.task = e.target.value 
+                        }
+                    })
+                    localStorage.setItem('todos',JSON.stringify(todos))
+                    span.children[0].setAttribute('readonly',true)
+                    displayTodos()
+                })
             })
         })
         
